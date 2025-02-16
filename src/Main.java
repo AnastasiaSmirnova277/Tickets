8fc5ddf91958f3d3c21b781d8ccee842786dabb7
Anastasia Smirnova <dmnast90@gmail.com>
2025-02-16 15:11:04 +0300

    changed

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 9365;
+        int equivalent = 20;
+        int bonus = amount / equivalent;
+        System.out.println(bonus);
+    }
+}
