9a15fb515bf7ef478f0aaf06ed7b098938cdad44
Anastasia Smirnova <dmnast90@gmail.com>
2025-02-22 16:35:18 +0300

    changed

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,7 @@
 public class BonusMilesService {
-    int price = 10_000;
-    int miles = service.calculate(price); // должно получиться 500
-        System.out.println(miles);
+    public int calculate(int price) {
+        int miles = price / 20;
+        return miles;
+        }
 }
 
